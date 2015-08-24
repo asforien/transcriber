edc0b6e1a573eda85133a43aefd25574e8970c8c
Jeremy Yap <asforien@gmail.com>
2015-08-24 11:30:33 +0800

    Comment out database check for audio file

--- interface/views.py
+++ interface/views.py
@@ -8,8 +8,8 @@ from django.conf import settings
 from .models import Audio
 
 def index(request, audio_file, distinctive_feature):
-	if not Audio.objects.all().filter(file_path=audio_file).exists():
-		return HttpResponseNotFound('<h1>Audio file not found</h1>')
+	#if not Audio.objects.all().filter(file_path=audio_file).exists():
+	#	return HttpResponseNotFound('<h1>Audio file not found</h1>')
 
 	module_dir = os.path.dirname(__file__)  # get current directory
 	segment_file_path = os.path.join(module_dir, 'static/media/segments/' + audio_file + '.json')
