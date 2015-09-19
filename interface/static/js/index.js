58d5354d3442a3a13b8a58bd82000a0eaa57310f
Jeremy Yap <asforien@gmail.com>
2015-09-19 18:42:27 +0800

    UI overhaul - popup menus for transcription to reduce clutter

--- interface/static/js/index.js
+++ interface/static/js/index.js
@@ -2,14 +2,14 @@ var wavesurfer = Object.create(WaveSurfer);
 var regions = [];
 var selectedRegion = null;
 
-document.addEventListener('DOMContentLoaded', function () {
-	var regionColor1 = "rgba(0, 0, 0, 0.2)";
-	var regionColor2 = "rgba(0, 0, 0, 0.3)";
+$(function () {
+	var regionColor1 = "rgba(51, 102, 102, 0.4)";
+	var regionColor2 = "rgba(68, 153, 204, 0.4)";
 
 	wavesurfer.init({
 	    container: document.querySelector('#wave'),
-	    waveColor: 'blue',
-	    progressColor: 'purple',
+	    waveColor: '#888',
+	    progressColor: '#437',
 	});
 
 	wavesurfer.on('ready', function () {
@@ -19,14 +19,14 @@ document.addEventListener('DOMContentLoaded', function () {
 				end: segment[0] + segment[1],
 				drag: false,
 				resize: false,
-				color: index % 2 ? regionColor1 : regionColor2,
+				color: index % 2 == 0 ? regionColor1 : regionColor2,
 			});
 			regions[index] = region;
 		})
 
 		$("region").each(function() {
         	$(".transcription").eq(0).clone()
-        	.css('left', $(this).position().left + $(this).width() / 2 - 17)
+        	.css('left', $(this).position().left + $(this).width() / 2 - 10)
         	.appendTo("#transcriptions");
 		});
 		$(".transcription").eq(0).remove();
@@ -61,18 +61,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	$("form").submit(function(event) {
 		var results = [];
-		$(".transcription").each(function(i) {
-			var $radioBtn = $(this).find("label.active input");
-			var result = $radioBtn.val();
+		var complete = true;
+
+		$(".transcription-value").each(function(i) {
+			var result = $(this).html().trim();
 			
-			if (!result) {
-				alert("Segment " + i + " is not annotated!");
-				event.preventDefault();
-				return false;
+			if (result == "?") {
+				complete = false;
+				$(this).addClass("incomplete");
 			}
-			results.push($radioBtn.val());
+			results.push(result);
 		});
 		console.log(results.join(""));
 		$("#result").val(results.join(""));
+
+		if (!complete) {
+			alert("There are incomplete segments");
+			event.preventDefault();
+		}
+	});
+
+	$("#transcriptions").on("click", ".transcription-menu button", function() {
+		var result = $(this).html()
+		$(this).closest(".transcription").find(".transcription-value").html(result).removeClass("incomplete");
 	});
 });
