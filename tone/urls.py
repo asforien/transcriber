7e631f07c48fe6d66f4420da01134467037d6bd4
Jeremy Yap <jeremy.yapjl@gmail.com>
2016-03-14 01:33:44 +0800

    Merge code for old and new interfaces. Interface to use is now decided by the subject's language group

--- tone/urls.py
+++ tone/urls.py
@@ -3,15 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^alt$', views.alt_start, name='alt_start'),
-    url(r'^alt/survey$', views.alt_survey, name='alt_survey'),
-    url(r'^alt/(.+)/(.+)$', views.alt_transcribe, name='alt_transcribe'),
-    url(r'^alt/summary$', views.alt_summary, name='alt_summary'),
-
     url(r'^$', views.start, name='start'),
     url(r'^resume$', views.resume, name='resume'),
     url(r'^survey$', views.survey, name='survey'),
     url(r'^(.+)/(.+)$', views.transcribe, name='transcribe'),
     url(r'^end', views.end, name='end'),
-    url(r'^summary', views.summary, name='summary'),
+    url(r'^summary/(.+)', views.summary, name='summary'),
 ]
--- tone/views.py
+++ tone/views.py
@@ -18,6 +18,10 @@ def transcribe(request, subjectId, questionId):
 	audioId = subject.question_order.split(',')[int(questionId) - 1]
 	audio = Audio.objects.get(pk=audioId)
 
+	interface = subject.interface
+	practice_template = 'number_practice.html' if interface == 0 else 'feature_practice.html'
+	transcribe_template = 'number_transcribe.html' if interface == 0 else 'feature_transcribe.html'
+
 	if request.method == 'POST':
 		result = request.POST.get('result', '')
 		timeTaken = request.POST.get('timeTaken', '')
@@ -48,7 +52,7 @@ def transcribe(request, subjectId, questionId):
 			'audio_file_path': 'data/audio/0.wav',
 			'alignments': alignments,
 		}
-		return render(request, 'number_practice.html', context)
+		return render(request, practice_template, context)
 	else:
 
 		try:
@@ -71,7 +75,7 @@ def transcribe(request, subjectId, questionId):
 			'alignments': alignments,
 			'answers': previous_answer,
 		}
-		return render(request, 'number_transcribe.html', context)
+		return render(request, transcribe_template, context)
 
 def start(request):
 
@@ -144,8 +148,15 @@ def survey(request):
 		else:
 			questionOrder = "2,1," + str(q3)
 
+		# Use new interface if subject falls within one of the following language groups
+		# Or if they speak cantonese
+		new_interface_languages = ['English', 'Mandarin', 'Vietnamese']
+		interface = 0
+		if dominantLanguage in new_interface_languages or targetLanguage == True:
+			interface = 1
+
 		sub = Subject.objects.create(name=name, email=email, dominant_language=dominantLanguage,
-			other_languages=otherLanguages, target_language=targetLanguage, gender=gender, age=age, question_order=questionOrder)
+			other_languages=otherLanguages, target_language=targetLanguage, gender=gender, age=age, question_order=questionOrder, interface=interface)
 
 		return HttpResponseRedirect('/tone/' + str(sub.pk) + '/0')
 
@@ -164,12 +175,12 @@ def end(request):
 
 	return render(request, 'end.html')
 
-def summary(request):
+def summary(request, interface):
 	subject_list = []
 	language_groups = {}
 	answer_key = Audio.objects.values_list('answer', flat=True)
 
-	for sub in Subject.objects.filter(interface=0):
+	for sub in Subject.objects.filter(interface=int(interface)):
 		transcriptions = get_transcriptions(sub)
 		if not transcriptions:
 			continue
@@ -326,240 +337,3 @@ def send_email(sub):
 	server.login(username,password)
 	server.sendmail(fromaddr, toaddr, msg)
 	server.quit()
-
-# Functions mapping to alt interface routes
-
-def alt_transcribe(request, subjectId, questionId):
-
-	subject = Subject.objects.get(pk=subjectId)
-	audioId = subject.question_order.split(',')[int(questionId) - 1]
-	audio = Audio.objects.get(pk=audioId)
-
-	if request.method == 'POST':
-		result = request.POST.get('result', '')
-		timeTaken = request.POST.get('timeTaken', '')
-
-		score = 0
-		for c, a in zip(result, audio.answer):
-			if c == a:
-				score += 1
-
-		Transcription.objects.update_or_create(subject=subject, audio=audio,
-			defaults={'result': result, 'timeTaken': timeTaken, 'score': score})
-
-		if int(questionId) == 3:
-			t = threading.Thread(target=send_email, args=[subject])
-			t.setDaemon(False)
-			t.start()
-			return HttpResponseRedirect('/tone/end')
-		else:
-			return HttpResponseRedirect('/tone/alt/' + subjectId + '/' + str(int(questionId) + 1))
-
-	elif questionId == '0':
-
-		alignments_file_path = settings.STATIC_ROOT + '/data/alignments/0.json'
-		alignments = open(alignments_file_path, 'r').read()
-
-		context = {
-			'subject_id': subjectId,
-			'audio_file_path': 'data/audio/0.wav',
-			'alignments': alignments,
-		}
-		return render(request, 'feature_practice.html', context)
-	else:
-
-		try:
-			previous_transcription = Transcription.objects.get(subject=subject, audio=audio)
-		except:
-			previous_transcription = None
-			print("Error")
-
-		previous_answer = None
-		if previous_transcription:
-			previous_answer = previous_transcription.result
-
-		alignments_file_path = settings.STATIC_ROOT + '/data/alignments/' + audioId + '.json'
-		alignments = open(alignments_file_path, 'r').read()
-		context = {
-			'audio_file_path': 'data/audio/' + audioId + '.wav',
-			'subject_id': subjectId,
-			'question_id': questionId,
-			'prev_qn_id': str(int(questionId)-1),
-			'alignments': alignments,
-			'answers': previous_answer,
-		}
-		return render(request, 'feature_transcribe.html', context)
-
-def alt_start(request):
-
-	return render(request, 'alt_start.html')
-
-def alt_survey(request):
-
-	if request.method == 'POST':
-		cipherName = request.POST.get('encryptedName', '')
-		cipherEmail = request.POST.get('encryptedEmail', '')
-		dominantLanguage = request.POST.get('dominantLanguage', '')
-		otherLanguages = request.POST.get('otherLanguages', '')
-		targetLanguage = request.POST.get('targetLanguage', '') == 'on'
-		gender = request.POST.get('gender', '')
-		age = request.POST.get('age', '')
-
-		f = open(settings.STATIC_ROOT + '/rsa/private_key.pem', 'r')
-		key = RSA.importKey(f.read())
-		name = key.decrypt(b64decode(cipherName))
-		email = key.decrypt(b64decode(cipherEmail))
-		name = name.decode('utf-8').replace('\0', '').encode('utf-8')
-		name = name.decode('unicode-escape')
-		email = email.decode('utf-8').replace('\0', '').encode('utf-8')
-
-		# Generate questions for subject
-
-		assignedQns = (Subject.objects.filter(dominant_language=dominantLanguage)
-			.values_list('question_order', flat="True").distinct())
-		
-		num_questions = Audio.objects.all().count()
-
-		times_question_used = [0] * (num_questions + 1)
-		for qs in assignedQns:
-			times_question_used[int(qs.split(',')[2])] += 1
-
-		q3 = 3
-		for i in range(4, num_questions + 1):
-			if times_question_used[i] < times_question_used[q3]:
-				q3 = i
-
-		if Subject.objects.all().count() % 2 == 0:
-			questionOrder = "1,2," + str(q3)
-		else:
-			questionOrder = "2,1," + str(q3)
-
-		sub = Subject.objects.create(name=name, email=email, dominant_language=dominantLanguage,
-			other_languages=otherLanguages, target_language=targetLanguage, gender=gender, age=age, question_order=questionOrder)
-
-		return HttpResponseRedirect('/tone/alt/' + str(sub.pk) + '/0')
-
-	else:
-		defaultLanguages = ['English', 'Mandarin']
-		languages = Subject.objects.values_list('dominant_language', flat=True).distinct()
-		languageSet = set(languages)
-		languageSet.discard("Testing")
-		languages = list(set(defaultLanguages) | languageSet)
-		context = {
-			'DLs': languages
-		}
-		return render(request, 'alt_survey.html', context)
-
-def alt_summary(request):
-	subject_list = []
-	language_groups = {}
-	answer_key = Audio.objects.values_list('answer', flat=True)
-
-	tone_direction = " lrlfrl"
-
-	for sub in Subject.objects.filter(interface=1):
-		transcriptions = get_transcriptions(sub)
-		if not transcriptions:
-			continue
-
-		correct = 0
-		correct_direction = 0
-		total = 0
-		time = 0
-		q1q2_correct = 0
-		q1q2_total = 0
-		correct_by_tone = [0] * 6
-		total_by_tone = [0] * 6
-		for t in transcriptions:
-			time += t[2]
-			qn = t[0] - 1 # zero-based indexing
-			result = t[1]
-
-			# pad result with zeroes to match answer key length for zipping
-			if len(result) < len(answer_key[qn]):
-				result += '0' * (len(result) - len(answer_key[qn]))
-
-			for c, a in zip(result, answer_key[qn]):
-				tone_number = int(a) - 1
-				if c == a:
-					correct += 1
-				if qn == 0 or qn == 1:
-					total_by_tone[tone_number] += 1
-					if c == a:
-						q1q2_correct += 1
-						correct_by_tone[tone_number] += 1
-				if tone_direction[int(c)] == tone_direction[int(a)]:
-					correct_direction += 1;
-
-			total += len(answer_key[qn])
-			if qn == 0 or qn == 1:
-				q1q2_total += len(answer_key[qn])
-
-		score = correct / total
-
-		subject_list.append({
-			'subject': sub,
-			'score': int(score * 100),
-			'time': time,
-		})
-
-		dl = sub.dominant_language
-		if dl not in language_groups:
-			language_groups[dl] = {
-				'name': dl,
-				'subjects': 0,
-				'correct': 0,
-				'correct_direction': 0,
-				'total': 0,
-				'q1q2_correct': 0,
-				'q1q2_total': 0,
-				'correct_by_tone': [0] * 6,
-				'total_by_tone': [0] * 6,
-				'time': 0,
-				'scores': [],
-			}
-
-		lg = language_groups[dl]
-		lg['subjects'] += 1
-		lg['correct'] += correct
-		lg['correct_direction'] += correct_direction
-		lg['total'] += total
-		lg['q1q2_correct'] += q1q2_correct
-		lg['q1q2_total'] += q1q2_total
-		for i in range(0,6):
-			lg['correct_by_tone'][i] += correct_by_tone[i]
-			lg['total_by_tone'][i] += total_by_tone[i]
-		lg['time'] += time
-		lg['scores'].append(score)
-
-	language_group_list = []
-
-	for lang in language_groups:
-		lg = language_groups[lang]
-		lg['score'] = int(lg['correct'] / lg['total'] * 100)
-		lg['direction_score'] = int(lg['correct_direction'] / lg['total'] * 100)
-		lg['q1q2_score'] = int(lg['q1q2_correct'] / lg['q1q2_total'] * 100)
-		lg['time'] = lg['time'] // lg['subjects']
-		lg['score_by_tone'] = [int(x / y * 100) for x, y in zip(lg['correct_by_tone'], lg['total_by_tone'])]
-		language_group_list.append(language_groups[lang])
-
-		scores = lg['scores']
-		avg_score = sum(scores)/len(scores)
-		variance = 0
-		for score in scores:
-			diff = score - avg_score
-			variance += diff * diff
-		std_dev = (variance / len(scores)) ** 0.5
-		lg['std_err'] = "{0:.4f}".format(std_dev / (len(scores) ** 0.5))
-
-	subject_list = sorted(subject_list, key=lambda k: k['score'], reverse=True)
-	subject_list = sorted(subject_list, key=lambda k: k['subject'].dominant_language)
-	language_group_list = sorted(language_group_list, key=lambda k: k['score'], reverse=True)
-
-	context = {
-		'subjects': subject_list,
-		'language_groups': language_group_list,
-		'interface': 1,
-	}
-
-	return render(request, 'summary.html', context)
